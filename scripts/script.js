405613f058ffe5a21c647d9c417d8265876c4b2d
Gabriel Tavares Barsani <103264805+g-Barsani@users.noreply.github.com>
2024-01-28 17:59:49 -0300

    Style sidebar, searchbar and remove draggable objects

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// JavaScript to set the draggable property
+var everything = document.querySelectorAll("*");
+everything.forEach(function(everything) {
+   everything.draggable = false;
+});
+
+
+console.log("AAAAAAAAAAAAAAAA");
